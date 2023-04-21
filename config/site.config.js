5c86bf1a2fddb76a5191cb598df0383dc4d5fa27
thioqd <131439748+thioqd@users.noreply.github.com>
2023-04-21 14:06:44 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,8 +52,8 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-  //    name: 'GitHub',
-   //   link: 'https://github.com/',
+      name: 'GitHub',
+      link: 'https://github.com/',
     },
   ],
 
